dbc735115f7f632a9b8581ec8fb2573f8cac334b
Kuan-Wen Chen <kev888kev@gmail.com>
2019-05-17 14:55:18 +0800

    add url_maker module

--- data_fetching/config.py
+++ data_fetching/config.py
@@ -1,6 +1,3 @@
 START_DATE = "1996-01-01"
 END_DATE = "2019-04-01"
 R_KEY = "bN9aBd1PzA"
-
-STOCK_AI_URL = "https://stock-ai.com/ddl?s={symbol}&r=" + \
-    R_KEY + "&d1=" + START_DATE + "&d2=" + END_DATE
