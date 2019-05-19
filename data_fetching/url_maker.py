5a9639baae3b9e5980027c49bfac6f0926b34edc
Kuan-Wen Chen <kev888kev@gmail.com>
2019-05-19 09:54:35 +0800

    refactor

--- data_fetching/url_maker.py
+++ data_fetching/url_maker.py
@@ -1,8 +1,6 @@
 from config import *
-STOCK_AI_URL = "https://stock-ai.com/ddl?s={symbol}&r=" + \
-    R_KEY + "&d1=" + START_DATE + "&d2=" + END_DATE
 
 
 def make_url_stock_ai(symbol):
-    # find {sybmol} and replace with the value of symbol variable
-    return STOCK_AI_URL.replace("{symbol}", symbol, 1)
+    return "https://stock-ai.com/ddl?s=" + symbol+"&r=" + \
+        R_KEY + "&d1=" + START_DATE + "&d2=" + END_DATE
---
+++ data_fetching/utils/download_helper.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+
+def download_file(url, filename):
+    ''' Downloads file from the url and save it as filename '''
+    # check if file already exists
+    if not os.path.isfile(filename):
+        print('Downloading {file}'.format(file=filename))
+        response = requests.get(url)
+        # Check if the response is ok (200)
+        if response.status_code == 200:
+            # Open file and write the content
+            with open(filename, 'wb') as file:
+                # A chunk of 128 bytes
+                for chunk in response:
+                    file.write(chunk)
+            print("Success!")
+    else:
+        print('{file} already exists'.format(file=filename))
