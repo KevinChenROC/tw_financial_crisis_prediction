9d910d1b52aa741249b696bc4e1201409630cb8f
Kuan-Wen Chen <kev888kev@gmail.com>
2019-05-22 14:43:04 +0800

    Fix log rate bug

--- data_transform/tests/calc_rate_test.py
+++ data_transform/tests/calc_rate_test.py
@@ -1,19 +1,11 @@
 import pandas as pd
 import numpy as np
 
-from ..util.calc_rate_helper import calc_log_rates, calc_percentage_of
+from ..util.calc_rate_helper import calc_simple_rates, calc_percentage_of
 
 df = pd.read_csv("datasets/raw_data/bond_yield_indexes/TGBRATE.csv",
                  header=0, parse_dates=[0], index_col=0)
 df.iloc[:5, 0] = np.nan
-df["Growth rate"] = calc_log_rates(df['Value'])
-print("#test calc_log_rates")
+df["Change rate"] = calc_simple_rates(df['Value'])
+print("#test calc_simple_rates")
 print(df.head(10))
-
-gdp = pd.read_csv("datasets/raw_data/tw_macro_indicators/TWRGDPNTD.csv",
-                  header=0, parse_dates=[0], index_col=0)
-reserves = pd.read_csv("datasets/raw_data/tw_macro_indicators/TWFERC.csv",
-                       header=0, parse_dates=[0], index_col=0)
-print("\n#test calc_ percetage_of")
-reserves_gdp = calc_percentage_of(reserves['Value'], gdp['Value'])
-print(reserves_gdp.head(20))
--- data_transform/util/calc_rate_helper.py
+++ data_transform/util/calc_rate_helper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def calc_log_rates(series):  # return a series with the same indexes.
+def calc_simple_rates(series):  # return a series with the same indexes.
     start_index = 0
 
     # find the index of first non-NaN
@@ -11,10 +11,14 @@ def calc_log_rates(series):  # return a series with the same indexes.
             start_index = i+1
             break
 
-    values = series.tolist()
     rates = []
-    for i in range(start_index, len(values)):
-        rates.append(np.log(values[i]/values[i-1]))
+    for i in range(start_index, series.size):
+        if(np.isclose(series[i-1], 0)):
+            rate = 0
+        else:
+            rate = series[i]/series[i-1] - 1
+        rates.append(rate)
+
     return pd.Series(rates, index=series.index[start_index:])
 
 
