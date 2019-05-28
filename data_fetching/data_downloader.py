344a28e970b58b654102d96f2d543386c7c8bc36
Kuan-Wen Chen <kev888kev@gmail.com>
2019-05-28 18:56:53 +0800

    Refactor data_fetching

--- data_fetching/data_downloader.py
+++ data_fetching/data_downloader.py
@@ -1,17 +1,29 @@
-from utils.url_maker import make_url_stock_ai
-from utils.download_helper import download_file
+from .utils.url_maker import make_url_stock_ai
+from .utils.download_helper import download_file
 import os
 from os.path import isfile, join
 import pandas as pd
-import config
+from . import config
 
 # iterate through all symbols csv
 symbols_file_paths = [f for f in os.listdir(
     config.SYMBOLS_PATH) if isfile(join(config.SYMBOLS_PATH, f))]
 
 for path in symbols_file_paths:
-    print(path[:-4])
+    target_folder = config.RAW_DATA_PATH + path[:-4] + '/'
     symbols = pd.read_csv(config.SYMBOLS_PATH + path)
+
+    # Make a folder
+    try:
+        os.mkdir(target_folder)
+        print("Create a folder: " + target_folder)
+    except FileExistsError:
+        print("Donwload data's to " + target_folder)
+    except OSError:
+        print(OSError)
+
     for symbol in symbols.loc[:, 'symbol']:
+
+        # Download file to that folder
         download_file(make_url_stock_ai(symbol),
-                      config.RAW_DATA_PATH + path[:-4] + "/" + symbol + ".csv")
+                      target_folder + symbol + ".csv")
