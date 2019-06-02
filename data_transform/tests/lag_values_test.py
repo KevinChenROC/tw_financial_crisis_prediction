3ea6283b1ba333192d29c1c23f85a16faf4f7d8a
Kuan-Wen Chen <kev888kev@gmail.com>
2019-06-02 11:56:05 +0800

    Refactor lag values file

--- data_transform/tests/lag_values_test.py
+++ data_transform/tests/lag_values_test.py
@@ -3,12 +3,12 @@ import numpy as np
 
 from ..util.lag_value import calc_lag_values
 from ..util.calc_rate import calc_simple_rates
+from data_fetching import config
 
 
-def test_calc_lag_values(change_rates):
-    lag_n = 2
+def test_calc_lag_values(change_rates, symbol, lag_n):
     lag_values = calc_lag_values(
-        change_rates, lag_n, "TGBRATE")
+        change_rates, lag_n, symbol)
     lag_values = lag_values.dropna()
 
     for i in range(lag_n, len(lag_values)):
@@ -17,8 +17,14 @@ def test_calc_lag_values(change_rates):
 
 
 def run_tests():
-    df = pd.read_csv("datasets/raw_data/bond_yield_indexes/TGBRATE.csv",
-                     header=0, parse_dates=[0], index_col=0)
+    symbol = "Test"
+    df1 = pd.read_csv("datasets/raw_data/bond_yield_indexes/TGBRATE.csv",
+                      header=0, parse_dates=[0], index_col=0)
 
-    change_rates = calc_simple_rates(df['Value'])
-    test_calc_lag_values(change_rates)
+    test_calc_lag_values(calc_simple_rates(df1['Value']), symbol, 3)
+
+    df2 = pd.read_csv(config.STOCK_INDX_PATH + "^N225.csv",
+                      header=0, parse_dates=[0], index_col=0)
+    test_calc_lag_values(calc_simple_rates(df2['Close']), symbol, 3)
+
+    print("Lag values test finished")
