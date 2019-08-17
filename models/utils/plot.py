e673ca736e105f5402b196739cf5b94f5bd26bc3
Kuan-Wen Chen <kev888kev@gmail.com>
2019-08-17 12:25:20 +0800

    Refactr fc_model and its utils. Add LSTM baseline model

--- models/utils/plot.py
+++ models/utils/plot.py
@@ -6,7 +6,7 @@ def plot_train_validation_metric(history, train_metric, val_metric):
     acc = history.history[train_metric]
     epochs = range(1, len(acc)+1)
 
-    plt.figure()
+    plt.figure(figsize=(15, 6), dpi=70)
 
     plt.plot(epochs, acc, 'r', label=train_metric)
     plt.plot(epochs, val_acc, 'b', label=val_metric)
