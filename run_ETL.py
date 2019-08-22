fa49673f4a685c1cb69312c1840987a9615e1daf
Kuan-Wen Chen <kev888kev@gmail.com>
2019-08-22 12:52:58 +0800

    refactor ETL

--- run_ETL.py
+++ run_ETL.py
@@ -8,38 +8,35 @@ from config import *
 from data_transform.util.file_extension import remove_extension
 
 raw_dataset_paths = [STOCK_INDX_PATH, FOREX_PATH] + [BOND_INDX_PATH,
-                                                     MACRO_INDTRS_PATH]
+                                                     MACRO_INDTRS_PATH, BISR_PATH]
 
 # value columns for selecting close prices for each category of data
-val_columns = ['Close']*2 + ['Value'] * 2
+val_columns = ['Close']*2 + ['Value'] * 3
 
 data_to_store_path = DATA_FOR_MODELS_PATH + \
     "dataset_" + START_DATE + "_" + END_DATE + ".csv"
 
-
-if os.path.isfile(data_to_store_path):
-    print(data_to_store_path + "already exists.")
-else:
-
-    print("Preparing to create " +
+if not os.path.isfile(data_to_store_path):
+    print("Preparing to create new " +
           data_to_store_path)
     # remove folder recursively
+    print("Remove all raw data...")
     folder_list = [f for f in os.listdir(RAW_DATA_PATH)]
     for f in folder_list:
         shutil.rmtree(RAW_DATA_PATH+f)
 
-    print("\n========================================Download raw data.========================================\n")
-    download_raw_data(SYMBOLS_PATH, RAW_DATA_PATH,
-                      START_DATE, END_DATE, R_KEY)
+print("\n" + "="*15 + "Download raw data." + "="*15 + "\n")
+download_raw_data(SYMBOLS_PATH, RAW_DATA_PATH,
+                  START_DATE, END_DATE, R_KEY)
 
-    print("\n========================================Transform and store raw data========================================\n")
-    df_transformed = get_transformed_data(
-        raw_dataset_paths, val_columns, RAW_DATA_PATH, START_DATE, END_DATE, RETURN_DISTR_WINDOW, MARKET_CRASH_THRESHOLD)
+print("\n" + "="*15 + "Transform and store raw data" + "=" * 15 + "\n")
+df_transformed = get_transformed_data(
+    raw_dataset_paths, val_columns, RAW_DATA_PATH, START_DATE, END_DATE, RETURN_DISTR_WINDOW, MARKET_CRASH_THRESHOLD)
 
-    assert((len(df_transformed[df_transformed.Crisis == 1]) +
-            len(df_transformed[df_transformed.Crisis == 0])) == len(df_transformed))
+assert((len(df_transformed[df_transformed.Crisis == 1]) +
+        len(df_transformed[df_transformed.Crisis == 0])) == len(df_transformed))
 
-    # store this DF
-    store_dataframe(df_transformed, 'csv', data_to_store_path)
+# store this DF
+store_dataframe(df_transformed, 'csv', data_to_store_path)
 
 print("ETL finished")
