7ed4f62b2f8791f53214588d5a745714e1b2a5d4
Kuan-Wen Chen <kev888kev@gmail.com>
2019-08-22 12:32:29 +0800

    Add new categorie of symbols

--- config.py
+++ config.py
@@ -7,16 +7,18 @@ R_KEY = "bN9aBd1PzA"
 
 RETURN_DISTR_WINDOW = 201
 MARKET_CRASH_THRESHOLD = 0.03
-PAST_N_DAYS_LIST = [5, 20]
-NEXT_N_DAYS_LIST = [10]
 
 # paths below are relative to the folder 'tw_crisis_prediction'
 DATASETS_PATH = 'datasets/'
 RAW_DATA_PATH = "datasets/raw_data/"
 SYMBOLS_PATH = "datasets/symbols/"
+
+# paths for raw data folders
 STOCK_INDX_PATH = RAW_DATA_PATH + "stock_indexes/"
 FOREX_PATH = RAW_DATA_PATH + "forex/"
 BOND_INDX_PATH = RAW_DATA_PATH + "bond_yield_indexes/"
 MACRO_INDTRS_PATH = RAW_DATA_PATH + "macro_indicators_monthly/"
+BISR_PATH = RAW_DATA_PATH + "bisr/"
 
+# path for data to train and predict on
 DATA_FOR_MODELS_PATH = DATASETS_PATH + "data_for_models/"
